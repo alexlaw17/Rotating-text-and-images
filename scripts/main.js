62bbf8bff690ad5b8a6b4a75e61e7bc72ad23736
Alex Law <alexlaw17@gmail.com>
2015-06-12 02:38:24 -0400

    change some styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,28 @@
 //Array of words
 var words = [
- 'Bobby',
- 'Jacky',
- 'Tiffany',
- 'Alex' //If doing one word Add the last one to the element in the html
+    'Bobby',
+    'Jacky',
+    'Tiffany',
+    'Alex' //If doing one word Add the last one to the element in the html
 ];
 
 $(function() {
-	var index = 0; 
-	function rotateWords() {
-		$('#swap').slideDown('fast', function(){
-			$(this).text(words[index]);
-			$(this).slideDown('fast', function(){
-				if (index == words.length-1) {
-					index = 0;
-				}
-				else {
-					index++; //Add 1 to the index
-				}
-			});
-		});
-	}
- setInterval (rotateWords, 470);
+    var index = 0;
+
+    function rotateWords() {
+        $('#swap').slideDown('fast', function() {
+            $(this).text(words[index]);
+            $(this).slideDown('fast', function() {
+                if (index == words.length - 1) {
+                    index = 0;
+                } else {
+                    index++; //Add 1 to the index
+                }
+            });
+        });
+    }
+    setInterval(rotateWords, 470);
+
+
 
 });
